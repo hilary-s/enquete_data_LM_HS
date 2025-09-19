474e8d5603d9e8a47143676678c141faeef766b8
Hilary <somhilary@yahoo.com>
2025-09-19 14:29:12 +0200

    maj global 19/09/2025

--- global.R
+++ global.R
@@ -5,7 +5,8 @@ library(dplyr)      #Tidyvers : traitement des données
 library(ggplot2)    #Tidyvers : conception de graphique
 library(forcats)    #Tidyvers : utilisation de facteurs
 library(lubridate)  #Tidyvers : traitement des dates
-
+library(stringr)
+library(tidyr)
 
 ############ Import des données ############
 data_enquete <- read_csv2("Data/enquete_data_raw.csv")
@@ -27,5 +28,32 @@ data_enquete <- data_enquete %>%
     data_enquete <- data_enquete %>%    
       select(-starts_with("B1"))
 
-#Création de la variable poste
+#Renommage des valeurs de la variable A3_poste
+    data_enquete <- data_enquete %>%
+      mutate(
+        A3_poste = case_when(
+          A3_poste == "Chargé d’études / chargé d’études statistiques" ~ "Data Analyst / analyste",
+          A3_poste == "Statisticien" ~ "Data Scientist",
+          TRUE ~ A3_poste
+        )
+      )
+    #vérification
+    data_enquete %>% count(B1_utilisation_powerBI)   
+    
+#Traitements des questions préfixées par B2_
+    b2_cols <- names(data_enquete)[str_detect(names(data_enquete), "^B2_")]
+    
+    data_enquete <- data_enquete %>%
+      pivot_longer(
+        cols = all_of(b2_cols),   # seulement les colonnes B2 passent en long
+        names_to = "outil",
+        values_to = "frequence"
+      ) %>%
+      mutate(
+        outil = str_remove(outil, "^B2_frequence_utilisation_"),      # enlever le préfixe b2_
+        frequence = replace_na(frequence, "Jamais")
+      )
+    
+    #vérification
+    data_enquete %>% count(outil) 
     
